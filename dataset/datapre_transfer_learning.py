71d307a077772234981560164135b59a6981f058
Jizhou Yu <boatyyu@outlook.com>
2024-07-03 14:29:34 +0800

    迁移区域修改

--- dataset/datapre_transfer_learning.py
+++ dataset/datapre_transfer_learning.py
@@ -13,7 +13,7 @@ import pandas as pd
 import numpy as np
 
 # Folder path
-folder_path = r'C:\Users\boaty\Documents\WHU Files\硕士项目\综合能源场景生成\Renewable Scenario Generation\dataset\ca-pv-2006'  # replace it with your folder path
+folder_path = r'dataset\ca-pv-2006'  # replace it with your folder path
 
 # The pattern which will be used to match the file names
 pattern = r"_([0-9.]+)_(-?[0-9.]+)_(\d{4})_(UPV|DPV)_"
@@ -47,38 +47,36 @@ longitudes_UPV = [longitudes[i] for i in range(len(tech_types)) if tech_types[i]
 latitudes_DPV = [latitudes[i] for i in range(len(tech_types)) if tech_types[i] == 'DPV']
 longitudes_DPV = [longitudes[i] for i in range(len(tech_types)) if tech_types[i] == 'DPV']
 
-# Plotting
-fig, ax = plt.subplots(figsize=(10, 6))
-ax.scatter(latitudes_DPV, longitudes_DPV, color='#E73744', label='DPV', marker='^', s=10)
+# # Plotting
+# fig, ax = plt.subplots(figsize=(10, 6))
+# ax.scatter(latitudes_DPV, longitudes_DPV, color='#E73744', label='DPV', marker='^', s=10)
 
-# Rectangle coordinates and dimensions
-rectangle_black = patches.Rectangle((33.4, -119.4), 1.1, 2.6, linewidth=1, edgecolor='black', facecolor='none')
-ax.add_patch(rectangle_black)
+# # Rectangle coordinates and dimensions
+# rectangle_black = patches.Rectangle((33.4, -119.4), 1.1, 2.6, linewidth=1, edgecolor='black', facecolor='none')
+# ax.add_patch(rectangle_black)
 
-rectangle_green1 = patches.Rectangle((32.5, -117.5), 0.8, 0.8, linewidth=1, edgecolor='green', facecolor='none')
-ax.add_patch(rectangle_green1)
+# rectangle_green1 = patches.Rectangle((32.5, -117.5), 0.8, 0.8, linewidth=1, edgecolor='green', facecolor='none')
+# ax.add_patch(rectangle_green1)
 
-rectangle_green2 = patches.Rectangle((35.2, -119.5), 0.3, 0.8, linewidth=1, edgecolor='green', facecolor='none')
-ax.add_patch(rectangle_green2)
+# rectangle_green2 = patches.Rectangle((35.2, -119.5), 0.3, 0.8, linewidth=1, edgecolor='green', facecolor='none')
+# ax.add_patch(rectangle_green2)
 
-ax.set_xlabel('纬度')
-ax.set_ylabel('经度')
-ax.set_title('加利福尼亚州光伏场站地理分布')
-plt.savefig(r'C:\Users\boaty\Documents\WHU Files\硕士项目\综合能源场景生成\Renewable Scenario Generation\dataset\ca-pv-2006\ca.jpg', dpi=300)
+# ax.set_xlabel('纬度')
+# ax.set_ylabel('经度')
+# ax.set_title('加利福尼亚州光伏场站地理分布')
+# plt.savefig(r'C:\Users\boaty\Documents\WHU Files\硕士项目\综合能源场景生成\Renewable Scenario Generation\dataset\ca-pv-2006\ca.jpg', dpi=300)
 
-plt.show()
+# plt.show()
 
 # Function to check if a point is inside a rectangle
 def is_inside_rectangle(lat, lon, rect):
     return rect[0] <= lat <= rect[0] + rect[2] and rect[1] <= lon <= rect[1] + rect[3]
 
 # Define rectangles for green boxes
-green_rect1 = (32.5, -117.5, 0.8, 0.8)
-green_rect2 = (35.2, -119.5, 0.3, 0.8)
+green_rect1 = (33.5, -117.4, 0.7, 0.6)
 
 # Lists to store data for each green rectangle
 green_data1 = []
-green_data2 = []
 
 # 读取CSV文件并处理数据，将时间颗粒度改为15分钟
 for file_name in os.listdir(folder_path):
@@ -92,8 +90,6 @@ for file_name in os.listdir(folder_path):
             df = pd.read_csv(os.path.join(folder_path, file_name))
             if is_inside_rectangle(latitude, longitude, green_rect1):
                 green_data1.append(df)
-            elif is_inside_rectangle(latitude, longitude, green_rect2):
-                green_data2.append(df)
 
 # 处理数据，将每小时数据扩展为15分钟间隔
 def process_data(data):
@@ -107,8 +103,6 @@ def process_data(data):
     return np.array(data_15min)
 
 green_data1_15min = process_data(green_data1)
-green_data2_15min = process_data(green_data2)
 
 # Save the processed data to npy files
-np.save(folder_path + r'\green_data1_15min.npy', green_data1_15min)
-np.save(folder_path + r'\green_data2_15min.npy', green_data2_15min)
+np.save(folder_path + r'\transfer_data_15min_new.npy', green_data1_15min)
